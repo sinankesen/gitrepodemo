99a0ae56114e90b75c20ab9e0e0301bb0b12f9b6
smk <sinankesen@hotmail.com>
2016-11-11 11:08:57 +0300

    added calculate.c

---
+++ Test/Test/Calculate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int Sum(int a, int b)
+{
+	return ( a + b ) ;
+}
+
+int Multiple(int a, int b) 
+{
+	return ( a * b );
+}
+
+int Div(int a, int b) 
+{
+	return ( a / b );
+}
--- Test/Test/main.c
+++ Test/Test/main.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+	int i = 0;
 	printf("Hello git\n");
 	return 0;
 }
